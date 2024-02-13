3dc73e21d5176e56c88a03744534942d975de48a
cybta <c.saad@live.com>
2024-02-13 23:37:05 +0300

    fixing colors

--- app/components/SidePannel.tsx
+++ app/components/SidePannel.tsx
@@ -26,13 +26,13 @@ const SidePannel = () => {
   };
 
   return (
-    <div className='bg-white w-[20%] flex flex-col p-[30px]'>
+    <div className='bg-white dark:bg-[#222222] w-[20%] flex flex-col p-[30px]'>
       {links && links.length > 0 ? (
         <div className='max-h-[850px] overflow-y-auto'>
           <div className='flex gap-[10px]'>
             {links.map((categ, i) => (
               <button
-                className='rounded-full bg-neutral-30 px-4 py-2  hover:text-secondary hover:bg-secondary-10'
+                className='rounded-full bg-neutral-30 px-4 py-2  hover:text-secondary hover:bg-secondary-10 text-primary-20'
                 key={i}
                 onClick={() => changeCategory(i)}
               >
@@ -48,10 +48,10 @@ const SidePannel = () => {
                 href={categ.url}
                 target='_blank'
                 rel='noopener noreferrer'
-                className={`py-5 text-primary-50 hover:text-secondary ${
+                className={`py-5 text-primary-50 dark:text-primary-10 hover:text-secondary dark:hover:text-secondary-10 ${
                   i === links[activeCategoryIndex].category_list.length - 1
                     ? ''
-                    : 'border-b border-b-neutral-30'
+                    : 'border-b border-b-neutral-30 dark:border-b-neutral-90'
                 } `}
               >
                 {categ?.name}
